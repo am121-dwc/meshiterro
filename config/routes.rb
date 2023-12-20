4c55f165e97983d658437e4994196b0b7ec62568
am121-dwc <aoimorisugi121@gmail.com>
2023-12-20 05:41:15 +0000

    chapter_16_finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show, :edit]
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
   devise_for :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
 root to: "homes#top"
   get "homes/about" => 'homes#about', as: 'about'
 end
